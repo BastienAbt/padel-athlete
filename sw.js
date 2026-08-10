d531009461978c1d4c5e1e2fcda384151d665e76
BastienAbt <8djpb24gkh@privaterelay.appleid.com>
2026-08-10 15:52:04 +0200

    Refresh PWA cache for V11

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="padel-athlete-v10";
-const ASSETS=["./index.html","./styles.css","./data.js","./app.js","./patch-v6.js","./patch-v7.js","./patch-v8.js","./patch-v9.js","./patch-v10.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="padel-athlete-v11";
+const ASSETS=["./index.html","./styles.css","./data.js","./app.js","./patch-v6.js","./patch-v7.js","./patch-v8.js","./patch-v9.js","./patch-v10.js","./patch-v11.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener("fetch",e=>{const req=e.request;if(req.mode==="navigate"){e.respondWith(fetch(req,{cache:'no-store'}).catch(()=>caches.match("./index.html")));return;}e.respondWith(caches.match(req).then(r=>r||fetch(req)));});
