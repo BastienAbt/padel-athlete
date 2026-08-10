00d1f1debe579b48ae2651b939c960c01449d0bc
BastienAbt <8djpb24gkh@privaterelay.appleid.com>
2026-08-10 10:50:31 +0200

    Bump PWA cache for V6

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-
-const CACHE="padel-athlete-v5";
-const ASSETS=["./","./index.html","./styles.css","./data.js","./app.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="padel-athlete-v6";
+const ASSETS=["./","./index.html","./styles.css","./data.js","./app.js","./patch-v6.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
